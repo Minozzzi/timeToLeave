956383e88f3897555d46ceabb95e01124bdd42d6
Guilherme Minozzi <minozzi60@gmail.com>
2023-06-12 20:29:26 -0300

    fix: app build

--- android/app/src/main/java/com/timetoleave/MainActivity.java
+++ android/app/src/main/java/com/timetoleave/MainActivity.java
@@ -17,9 +17,9 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-    * (aka React 18) with two boolean flags.
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
