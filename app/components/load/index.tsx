18783cb14d46abd88616d8ccd1d91b6ff42e9f26
Guilherme Minozzi <minozzi60@gmail.com>
2023-06-16 20:11:47 -0300

    feat(components): add loading

---
+++ app/components/load/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { ActivityIndicator } from 'react-native'
+import { useTheme } from 'styled-components'
+
+export const Load = () => {
+  const theme = useTheme()
+
+  return (
+    <ActivityIndicator
+      color={theme.COLORS.PRIMARY}
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    />
+  )
+}
