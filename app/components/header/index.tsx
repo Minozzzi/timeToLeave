0583a0594c80d852b8fc675f108f381b7276f44f
Guilherme Minozzi <minozzi60@gmail.com>
2023-06-16 19:55:21 -0300

    feat: add header

---
+++ app/components/header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container, Greeting, Title, SubTitle } from './styles'
+import { IconButton } from '../iconButton'
+
+export const Header: React.FC = () => (
+  <Container>
+    <Greeting>
+      <Title>Time to leave</Title>
+      <SubTitle>Saiba a hora de sair do trabalho.</SubTitle>
+    </Greeting>
+
+    <IconButton icon="logout" />
+  </Container>
+)
